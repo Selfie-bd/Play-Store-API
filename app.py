580a9d6ead500cb63ba22a31b9bbb64f2ab88461
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-09-16 19:01:42 +0530

    feature: added app details (#2)

--- app.py
+++ app.py
@@ -16,6 +16,23 @@ def main():
     return "Documentation:- <a href='https://github.com/FayasNoushad/Play-Store-API'>Play-Store-API</a>"
 
 
+@app.route("/details/", methods=['GET'])
+def app_details():
+    query = request.args.get('query')
+    if query is not None:
+        details = details(query)
+        if details is None:
+            return jsonify(
+                {"error": "No details found"}
+            )
+        if details is not None:
+            return jsonify(details)
+    else:
+        return jsonify(
+            {"error": "Query is None"}
+        )
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host="0.0.0.0", port=5000, use_reloader=True, threaded=True)
